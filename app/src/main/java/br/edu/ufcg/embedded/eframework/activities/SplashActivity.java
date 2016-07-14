eff74c069182458ef83e1ba1ca21b8e7acc2e1b2
bruno-rocha <bruno.rocha@ccc.ufcg.edu.br>
2016-07-14 13:28:33 -0300

    fist commit

---
+++ app/src/main/java/br/edu/ufcg/embedded/eframework/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ufcg.embedded.eframework.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ufcg.embedded.eframework.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
