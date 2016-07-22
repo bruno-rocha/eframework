724d61e22e4a7fda71a5ea97edb6e2b1d6a88055
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2016-07-22 10:57:55 -0300

    mudando icone downloader, adicionando dialogs

--- app/src/main/java/br/edu/ufcg/embedded/eframework/utils/EventsAdapter.java
+++ app/src/main/java/br/edu/ufcg/embedded/eframework/utils/EventsAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +31,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
         public TextView itemTitle;
         public CardView cardView;
         public RecyclerView recyclerView;
+        public ImageButton itemDownloader;
 
         public EventsHolder(View itemView) {
             super(itemView);
@@ -43,6 +45,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
             cardView = (CardView) itemView.findViewById(R.id.cardView);
             recyclerView = (RecyclerView) itemView.findViewById(R.id.recyclerview);
             img = (ImageView) itemView.findViewById(R.id.cardImage);
+            itemDownloader = (ImageButton) itemView.findViewById(R.id.cardDownload);
 
 
         }
@@ -72,13 +75,13 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
             public void onClick(View v) {
                 AlertDialog.Builder dialog = new AlertDialog.Builder(context);
                 dialog.setCancelable(false);
-                dialog.setPositiveButton("Instalar", new DialogInterface.OnClickListener() {
+                dialog.setPositiveButton(context.getString(R.string.install), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(context, "Instalando evento...", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, context.getString(R.string.installing), Toast.LENGTH_SHORT).show();
                     }
                 });
-                dialog.setNegativeButton("cancelar", new DialogInterface.OnClickListener() {
+                dialog.setNegativeButton(context.getString(R.string.cancel), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -88,6 +91,13 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventsHold
                 dialog.setMessage(eventos.get(i).getDescricao());
                 dialog.create();
                 dialog.show();
+            }
+        });
+        viewHolder.itemDownloader.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                        Toast.makeText(context, context.getString(R.string.installing), Toast.LENGTH_SHORT).show();
+
             }
         });
 
